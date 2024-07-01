b4cc1b12f8c2d0edc598e75c93050fdb0b607d29
waimea-ppiasupan <168692134+waimea-ppiasupan@users.noreply.github.com>
2024-07-01 13:31:25 +1200

    s

--- site/index.php
+++ site/index.php
@@ -10,22 +10,12 @@ include 'partials/top.php'; ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="color-scheme" content="light dark" />
     <link rel="stylesheet" href="css/styles.css">
-    <title>Hello </title>
+    <title>Hello yes</title>
   </head>
   <body>
     <main class="container">
       <h1></h1>
       <h2> New stuff</h2>
-
-<form method="post" action = "add-company.php">
-    <label>code</label>
-    <input name="code" type="text" placeholder="e.g. your mum's name" minlength="3" maxlength="3" pattern="[A-Z][A-z][A-Z]"required >
-    <label>name</label>
-    <input name="name" type="text"placeholder="yes" required>
-    <label>website</label>
-    <input name="website" placeholder="im an npc.com" type="url" required>
-    <input type="submit" value="add" required>
-</form>
 <h3>IFrame lol </h3>
 
     <iframe src="https://dt.waimea.school.nz" title="W3Schools Free Online Web Tutorials"></iframe>
